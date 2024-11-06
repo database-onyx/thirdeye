04138d0a8d2f02f8a75d5279b77e3b92ef64fbdf
database-onyx <141583093+database-onyx@users.noreply.github.com>
2024-11-06 21:56:37 +0530

    second-event-pinchange

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.get("/", (req, res) => {
 // Route for PIN authentication
 app.post("/", async (req, res) => {
   const pin = req.body.pin;
-  if (pin === "8825") {
+  if (pin === "7708") {
     res.redirect("/thirdeyeportal1");
   } else if (pin === "3933") {
     res.redirect("/thirdeyeportal2");
