7501017a90564b1effbf86bb0f89debf89a32285
database-onyx <141583093+database-onyx@users.noreply.github.com>
2024-12-08 07:56:12 +0530

    Update thirdeyeportal1_schema.js

--- model/thirdeyeportal1_schema.js
+++ model/thirdeyeportal1_schema.js
@@ -18,5 +18,5 @@ const imageSchema = new mongoose.Schema({
 });
 
 
-const ImageModel = mongoose.model("portal1", imageSchema, "portal1");
+const ImageModel = mongoose.model("portal4", imageSchema, "portal4");
 module.exports = ImageModel;
