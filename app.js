070c399ac6b59859d41e6bfdc022cd239087ed9d
database-onyx <141583093+database-onyx@users.noreply.github.com>
2025-02-16 10:13:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.get("/", (req, res) => {
 // Route for PIN authentication
 app.post("/", async (req, res) => {
   const pin = req.body.pin;
-  if (pin === "5454") {
+  if (pin === "7534") {
     res.redirect("/thirdeyeportal1");
   } else if (pin === "6464") {
     res.redirect("/thirdeyeportal2");
